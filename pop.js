e8bc7577911312577367933e49742f0e0fc77bc6
Akash <akash@trigvent.com>
2022-10-31 11:47:52 +0530

    files added

---
+++ pop.js
@@ -0,0 +1,13 @@
+
+window.onload = function () {
+    window.open("http://localhost/akash_php_work/addRecord.php");
+    // chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+    //     console.log(tabs)
+    //     var activeTab = tabs[0];
+    //     if (activeTab.url.indexOf("facebook.com") != -1) {
+    //         chrome.tabs.sendMessage(activeTab.id, {"action":"check"});
+    //     } else {
+           
+    //     }
+    // })
+}
